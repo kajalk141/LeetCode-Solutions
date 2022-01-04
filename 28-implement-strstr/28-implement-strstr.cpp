07245e36f7c61765bd44e4a34c1413e6523c0790
kajalk14 <88448547+kajalk14@users.noreply.github.com>
2022-01-05 00:53:39 +0530

    Time: 7 ms (84.66%), Space: 8.9 MB (15.39%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int n=haystack.size(), m=needle.size();
+    int strStr(string text, string pattern) {
+        int n=text.size(), m=pattern.size();
+        
+        if(n<m) return -1;
         if(m==0) return 0;
+        
+        int p=31, mod=1e9+9;
+        vector<long long> p_pow(n);
+        p_pow[0]=1;
+        for(int i=1; i<(int)p_pow.size(); i++)
+            p_pow[i]=(p_pow[i-1]*p)%mod;
+        
+        vector<long long> hash(n+1,0);
+        for(int i=0; i<n; i++)
+            hash[i+1]=(hash[i]+(text[i]-'a'+1)*p_pow[i])%mod;
+        
+        long long hash_pat=0;
+        for(int i=0; i<m; i++)
+            hash_pat=(hash_pat+(pattern[i]-'a'+1)*p_pow[i])%mod;
+        
         for(int i=0; i<=n-m; i++){
-            bool flag=true;
-            for(int j=0; j<m; j++){
-                if(haystack[i+j]!=needle[j]){
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag==true) return i;
+            long long curr= (hash[i+m]-hash[i]+mod)%mod;
+            if(curr==(hash_pat*p_pow[i])%mod)
+                return i;
         }
         return -1;
     }
