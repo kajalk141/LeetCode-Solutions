9701d2110cf13e9f6c5012d26fd9e0943637b27f
kajalk14 <88448547+kajalk14@users.noreply.github.com>
2022-06-21 04:14:14 +0530

    Time: 747 ms (62.16%), Space: 12.9 MB (75.52%) - LeetHub

---
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -0,0 +1,55 @@
+int m,n;
+class Solution {
+    struct trie{
+        trie *child[26];
+        int end=0;
+        string word="";
+    };
+    trie *root;
+public:
+    vector<string> res;
+    Solution(){
+        root=new trie();
+    }
+    void insert(string s){
+        trie *temp=root;
+        for(char c:s){
+            if(!temp->child[c-'a']) temp->child[c-'a']=new trie();
+            temp=temp->child[c-'a'];
+        }
+        temp->end++;
+        temp->word=s;
+    }
+    bool check(int i, int j, vector<vector<char>>& board){
+        if(i<0||i>=m||j<0||j>=n||board[i][j]=='.') return false;
+        return true;
+    }
+    void rec(int i, int j, vector<vector<char>>& board, trie *curr){
+        if(curr->child[board[i][j]-'a']==NULL) return;
+        curr=curr->child[board[i][j]-'a'];
+        if(curr->end){
+            res.push_back(curr->word);
+            curr->end--;
+        }
+        char temp=board[i][j];
+        board[i][j]='.';
+        if(check(i+1,j,board)) rec(i+1,j,board,curr);
+        if(check(i-1,j,board)) rec(i-1,j,board,curr);
+        if(check(i,j+1,board)) rec(i,j+1,board,curr);
+        if(check(i,j-1,board)) rec(i,j-1,board,curr);
+        board[i][j]=temp;
+    }
+    
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        res.clear();
+        for(string word:words) insert(word);
+        m=board.size(); n=board[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                // if(root->child[board[i][j]-'a']!=NULL)
+                rec(i,j,board,root);
+            }
+        }
+        return res;
+    }
+};
