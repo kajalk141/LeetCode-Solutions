fab6c57db55216dd909f4a9648f33cbd37ad5b4c
kajalk14 <88448547+kajalk14@users.noreply.github.com>
2022-07-10 20:07:05 +0530

    Time: 10 ms (86.92%), Space: 9.1 MB (38.38%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,20 +1,20 @@
 class Solution {
     int rec(int i, int j, string &s, string &t, vector<vector<int>> &dp){
-        if(i<0 || j<0) return abs(i-j);
+        if(i<0||j<0) return abs(i-j);
         if(dp[i][j]!=-1) return dp[i][j];
         int ans=INT_MAX;
-        if(s[i]==t[j]) ans=rec(i-1,j-1,s,t,dp);
+        if(s[i]==t[j]) ans=rec(i-1, j-1, s, t, dp);
         else{
-            ans=min(ans, 1+rec(i-1,j,s,t,dp)); //deleting
-            ans=min(ans, 1+rec(i,j-1,s,t,dp)); //inserting
-            ans=min(ans, 1+rec(i-1,j-1,s,t,dp)); //replacing
+            ans=min(ans, 1+rec(i, j-1, s, t, dp)); //insert
+            ans=min(ans, 1+rec(i-1, j, s, t, dp)); //delete
+            ans=min(ans, 1+rec(i-1, j-1, s, t, dp)); //replace
         }
         return dp[i][j]=ans;
     }
 public:
-    int minDistance(string word1, string word2) {
-        int n=word1.size(), m=word2.size();
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return rec(n-1,m-1,word1,word2,dp);
+    int minDistance(string s, string t) {
+        int n=s.size(), m=t.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        return rec(n-1, m-1, s, t, dp);
     }
 };
