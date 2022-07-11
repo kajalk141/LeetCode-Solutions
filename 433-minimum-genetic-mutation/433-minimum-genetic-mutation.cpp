b17e1aa770b6bfa5ce47076f49c1aa37948a5c14
kajalk14 <88448547+kajalk14@users.noreply.github.com>
2022-07-11 21:41:18 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (5.87%) - LeetHub

---
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minMutation(string begin, string end, vector<string>& List) {
+        map<string, set<string>> mp;
+        for(int i=0; i<(int)begin.size(); i++){
+            string str=begin.substr(0,i)+'*'+begin.substr(i+1);
+            mp[str].insert(begin);
+        }
+        for(string word:List){
+            for(int i=0; i<(int)word.size(); i++){
+                string str=word.substr(0,i)+'*'+word.substr(i+1);
+                mp[str].insert(word);
+            }
+        }
+        map<string, bool> visited;
+        queue<string> q;
+        int len=0;
+        visited[begin]=1;
+        q.push(begin);
+        while(!q.empty()){
+            len++;
+            int sz=q.size();
+            for(int i=0; i<sz; i++){
+                string word=q.front();
+                q.pop();
+                if(word==end) return len-1;
+                for(int c=0; c<(int)word.size(); c++){
+                    string str=word.substr(0,c)+'*'+word.substr(c+1);
+                    for(string s:mp[str]){
+                        if(visited[s]==1) continue;
+                        visited[s]=1;
+                        q.push(s);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
