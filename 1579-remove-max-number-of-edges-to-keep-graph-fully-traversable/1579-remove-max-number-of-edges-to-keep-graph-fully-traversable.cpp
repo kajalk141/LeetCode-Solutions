3495693ad5bd1d96ffb1e8b0216c2a632a445969
kajalk14 <88448547+kajalk14@users.noreply.github.com>
2022-07-22 23:59:02 +0530

    Time: 1224 ms (24.32%), Space: 139.8 MB (58.21%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -11,10 +11,10 @@ public:
     bool merge(int a, int b) {       
         a=find(a);
         b=find(b);
-        if(a==b) return 0;
+        if(a==b) return 1;
         par[b]=a;
         cc--;
-        return 1;
+        return 0;
     }
     int find(int a) {
         if(a==par[a]) return a;
@@ -30,26 +30,23 @@ class Solution {
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
         sort(edges.begin(), edges.end(), [] (auto &a, auto &b) { return a[0] > b[0]; });
-        
-        int edgesAdded = 0; 
-        
+        int cnt = 0; 
         UnionFind bob(n), alice(n); 
-        
         for (auto &edge: edges) { 
             int type = edge[0], one = edge[1], two = edge[2];
             switch(type) {
                 case 3:
-                    edgesAdded += (bob.merge(one, two) | alice.merge(one, two));
+                    cnt += (bob.merge(one, two) || alice.merge(one, two));
                     break;
                 case 2:
-                    edgesAdded += bob.merge(one, two);
+                    cnt += bob.merge(one, two);
                     break;
                 case 1:
-                    edgesAdded += alice.merge(one, two);
+                    cnt += alice.merge(one, two);
                     break;
             }
         }
         
-        return (bob.united() && alice.united()) ? (edges.size()-edgesAdded) : -1; 
+        return (bob.united() && alice.united()) ? cnt : -1; 
     }
 };
